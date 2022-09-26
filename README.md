c90e84697d32a45fbc832889016e519620759a31
Savvas Vezyridis <svezyridis@gmail.com>
2022-09-26 17:32:09 +0300

    ngrok bot

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ngrok-url-bot
+
+This is a bot that checks for current ngrok url on system and publishes it on a slack channel
+
+It requires 2 env variables:
+
+$SLACK_TOKEN
+
+and
+
+$SLACK_CHANNEL
---
+++ ngrokURL.js
@@ -0,0 +1,42 @@
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const cron = require("node-cron");
+
+const postURL = () => {
+  fetch("http://localhost:4040/api/tunnels")
+    .then((res) => res.json())
+    .then((json) => json.tunnels.find((tunnel) => tunnel.proto === "tcp"))
+    .then((secureTunnel) => {
+      console.log(secureTunnel.public_url);
+      //send url to slack channel
+      fetch("https://slack.com/api/chat.postMessage", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
+        },
+        body: JSON.stringify({
+          channel: process.env.SLACK_CHANNEL,
+          text: `ngrok url:${secureTunnel.public_url}`,
+        }),
+      });
+    })
+    .catch((err) => {
+      if (err.code === "ECONNREFUSED") {
+        fetch("https://slack.com/api/chat.postMessage", {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
+          },
+          body: JSON.stringify({
+            channel: process.env.SLACK_CHANNEL,
+            text: "ngrok is not running",
+          }),
+        });
+      }
+      console.error(err);
+    });
+};
+postURL();
+cron.schedule("0 */2 * * *", postURL);
